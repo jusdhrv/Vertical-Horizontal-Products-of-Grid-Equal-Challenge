ce77d8dbf41f442892152ddc7263c587fa38ee62
jusdhrv <20d.dhruv@gmail.com>
2025-01-12 13:40:40 +0530

    Optimize execution - add parallelization and memoization to list multiples and indices.

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-import math, time
-from datetime import timedelta
+import itertools
+import math
+import time
+from multiprocessing import Pool, cpu_count
 
-
-def format_time(sec):
-    td = timedelta(seconds=sec)
-    return td
-
-
-def permutation(lst):
-    if len(lst) == 0:
-        return []
-    if len(lst) == 1:
-        return [lst]
-    l = []
-    for i in range(len(lst)):
-        m = lst[i]
-        remLst = lst[:i] + lst[i + 1 :]
-        for p in permutation(remLst):
-            l.append([m] + p)
-    return l
+# Memoization dictionaries
+memo = {}
+index_memo = {}
 
 
 def list_multiple(lst):
@@ -28,78 +15,64 @@ def list_multiple(lst):
     return product
 
 
-def cleanup(lst):
-    lst1 = list(set(lst))
-    lst2 = sorted(lst1)
+def memoized_list_multiple(sublist):
+    key = tuple(sublist)
+    if key not in memo:
+        memo[key] = list_multiple(sublist)
+    return memo[key]
 
-    return lst2
+
+def memoized_indices(n):
+    if n not in index_memo:
+        start_indices = [j * n for j in range(n)]
+        end_indices = [start_index + n for start_index in start_indices]
+        row_indices = [
+            list(range(start, end)) for start, end in zip(start_indices, end_indices)
+        ]
+        col_indices = [[l + m * n for m in range(n)] for l in range(n)]
+        index_memo[n] = (row_indices, col_indices)
+    return index_memo[n]
 
 
 def log_append(data):
-    file1 = open("logs.txt", "a")
-    file1.write(data)
-    file1.write("\n")
-    file1.close()
+    with open("logs.txt", "a") as file1:
+        file1.write(data + "\n")
+
+
+def check_permutation(p, n):
+    row_indices, col_indices = memoized_indices(n)
+    h_product = [memoized_list_multiple([p[idx] for idx in row]) for row in row_indices]
+    v_product = [memoized_list_multiple([p[idx] for idx in col]) for col in col_indices]
+
+    if set(h_product) == set(v_product):
+        return str(p) + " " + str(h_product) + " " + str(v_product)
+    return None
 
 
 def find_grids_n(n):
     log_append("For, n = " + str(n))
-    possible_vals = []
-    for i in range(n * n):
-        possible_vals.append(int(i + 1))
+    possible_vals = list(range(1, n * n + 1))
 
     log_append("Possible values of the grid cells are: " + str(possible_vals) + "\n")
     n_start_time = time.time()
 
-    start_indices = []
-    end_indices = []
-    for j in range(0, n):
-        start_index = j * n
-        end_index = start_index + n
-        start_indices.append(start_index)
-        end_indices.append(end_index)
-
-    h_products = []
-    v_products = []
-
     total_p = math.factorial(n * n)
     p_count = 1
-    for p in permutation(possible_vals):
 
-        print(
-            "Executing Permutation "
-            + str(p_count)
-            + " of "
-            + str(total_p)
-            + " for n="
-            + str(n)
-            + "..."
+    valid_permutations = []
+
+    with Pool(cpu_count()) as pool:
+        results = pool.starmap(
+            check_permutation, [(p, n) for p in itertools.permutations(possible_vals)]
         )
-        p_count += 1
 
-        h_product = []
-        v_product = []
+    for result in results:
+        if result:
+            valid_permutations.append(result)
 
-        for k in range(0, n):
-            h_prod = list_multiple(p[start_indices[k] : end_indices[k]])
-            h_product.append(h_prod)
+    for valid_permutation in valid_permutations:
+        log_append(valid_permutation)
 
-        for l in range(0, n):
-            v_list = []
-            for m in range(0, n):
-                v_list.append(p[l + m * n])
-
-            v_prod = list_multiple(v_list)
-            v_product.append(v_prod)
-
-        s_h_product = sorted(h_product)
-        s_v_product = sorted(v_product)
-
-        h_products.append(s_h_product)
-        v_products.append(s_v_product)
-
-        if s_h_product == s_v_product:
-            log_append(str(p) + " " + str(h_product) + " " + str(v_product))
     log_append("\nExecution Time: " + str(format_time(time.time() - n_start_time)))
     log_append("\n---\n")
     print(
@@ -109,28 +82,42 @@ def find_grids_n(n):
     )
 
 
-try:
-    print(
-        "This programme executes the possible grid finder from 1 up to a maximum 'n' of your choice..."
-    )
-    n_max = int(input("Enter the value for 'n' to use: "))
-    main_start_time = time.time()
+def format_time(seconds):
+    return time.strftime("%H:%M:%S", time.gmtime(seconds))
 
-    if n_max < 0:
-        print("\nInvalid value provided. Must be a natural number")
 
-    elif n_max == 0:
-        grid_size = 1
-        while True:
-            find_grids_n(grid_size)
-            grid_size += 1
+# Example usage
+if __name__ == "__main__":
+    try:
+        print(
+            "This programme executes the possible grid finder from 1 up to a maximum 'n' of your choice..."
+        )
+        n_max = int(input("Enter the value for 'n' to use: "))
+        main_start_time = time.time()
 
-    elif n_max > 0:
-        for grid_size in range(1, n_max + 1):
-            find_grids_n(grid_size)
+        if n_max < 0:
+            print("\nInvalid value provided. Must be a natural number")
 
-    print("\nFinished execution...")
-    print("Total Execution Time: " + str(format_time(time.time() - main_start_time)))
+        elif n_max == 0:
+            grid_size = 1
+            while True:
+                try:
+                    find_grids_n(grid_size)
+                    grid_size += 1
+                except KeyboardInterrupt:
+                    print("\nExecution interrupted by user.")
+                    break
 
-except KeyboardInterrupt:
-    print("\nExiting the program...")
+        elif n_max > 0:
+            try:
+                for grid_size in range(1, n_max + 1):
+                    find_grids_n(grid_size)
+            except KeyboardInterrupt:
+                print("\nExecution interrupted by user.")
+
+        print(
+            "Total Execution Time: " + str(format_time(time.time() - main_start_time))
+        )
+
+    except ValueError:
+        print("\nInvalid input. Please enter a valid integer.")
