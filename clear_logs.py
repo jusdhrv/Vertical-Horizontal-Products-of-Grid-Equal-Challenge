e14b52eca1a74474e84b234caf7bd65fa03099bc
jusdhrv <20d.dhruv@gmail.com>
2025-07-13 20:43:43 +0530

    Add output clearing to utility programs

--- clear_logs.py
+++ clear_logs.py
@@ -12,6 +12,18 @@ def clear_logs():
     else:
         print(f"No logs to delete")
 
+def clear_outputs():
+    existing_outputs = [f for f in os.listdir("Data") if f.endswith("-output.json")]
+    if existing_outputs:
+        for output_file in existing_outputs:
+            output_file_path = f"Data/{output_file}"
+            os.remove(output_file_path)
+            print(f"Deleted {output_file_path}")
+        print(f"Cleared contents of outputs")
+    else:
+        print(f"No outputs to delete")
+
 
 if __name__ == "__main__":
     clear_logs()
+    clear_outputs()
